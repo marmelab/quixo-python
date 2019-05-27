c5367c0bd9168c309f734d4b73399e1f2cbc287f
Pierre Haller <pierre@marmelab.com>
2019-05-27 14:59:55 +0200

    Add newline to endfiles & before function definition

--- src/board.py
+++ src/board.py
@@ -1,12 +1,14 @@
 def create_board():
     return [[0 for j in range(5)] for i in range(5)]
 
+
 def get_symbol(value):
     if value < 0:
         return 'x'
     if value > 0:
         return 'o'
     return ' '
+
 
 def print_board(board):
     for line in board:
--- src/board_test.py
+++ src/board_test.py
@@ -2,6 +2,7 @@ import unittest
 
 from board import create_board
 
+
 class TestBoardMethods(unittest.TestCase):
 
     def test_empty_board(self):
