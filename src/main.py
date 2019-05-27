1f60984af77fff4079e0ccf259c6036619171712
Pierre Haller <pierre@marmelab.com>
2019-05-27 17:40:25 +0200

    Add __init__.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,4 @@ from board import create_board, print_board
 from moves import move_tile
 
 board = create_board()
-
-board = move_tile(board, (0, 0), (0, 4), 1)
-board = move_tile(board, (0, 0), (0, 4), 1)
-board = move_tile(board, (0, 0), (0, 4), 1)
-board = move_tile(board, (0, 0), (0, 4), 1)
-board = move_tile(board, (0, 0), (0, 4), -1)
-board = move_tile(board, (0, 4), (4, 4), -1)
-board = move_tile(board, (4, 0), (4, 4), 1)
-board = move_tile(board, (0, 4), (4, 4), -1)
 print_board(board)
