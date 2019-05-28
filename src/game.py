80f38d13ba05b1bf38239f50a5fd4e1c33ea535e
Pierre Haller <boiteu54@gmail.com>
2019-05-28 14:30:16 +0200

    Rename movables function

--- src/game.py
+++ src/game.py
@@ -4,7 +4,7 @@ import os
 
 clear = lambda : os.system('clear')
 
-def get_instructions(board, player_value=0):
+def get_movables_tiles(board, player_value=0):
     """Get movable tiles with an associated number
 
     Arguments:
@@ -14,21 +14,21 @@ def get_instructions(board, player_value=0):
     Returns:
         Dictionnary -- A dictionnary with coords in key and instruction in value
     """
-    instructions = {}
-    n_instruction = 1
+    movable = {}
+    n = 1
     for x in range(len(board)):
         for y in range(len(board[x])):
             value = board[x][y]
             if is_movable_tile(x, y) and (value == 0 or value == player_value):
-                instructions[(x, y)] = n_instruction
-                n_instruction += 1
+                movable[(x, y)] = n
+                n += 1
 
-    return instructions
+    return movable
 
 
-def get_coord_from_instruction(instructions, n):
-    for coord, instruction in instructions.items():
-        if instruction == n:
+def get_coords_from_movables(movables, n):
+    for coord, number in movables.items():
+        if number == n:
             return coord
 
     return None
@@ -39,11 +39,11 @@ def play():
     player_team = 1
     board = create_board()
     while True:
-        instructions = get_instructions(board, player_team)
-        print_board(board, instructions)
-        instruction = int(input('Which tile do you want to move ?'))
+        movables = get_movables_tiles(board, player_team)
+        print_board(board, movables)
+        player_input = int(input('Which tile do you want to move ?'))
         # check_input()
-        (x_start, y_start) = get_coord_from_instruction(instructions, instruction)
+        (x_start, y_start) = get_coords_from_movables(movables, player_input)
         (x_end, y_end) = get_opposite_tile(x_start, y_start)
         board = move_tile(board, (x_start, y_start), (x_end, y_end), player_team)
         player_team *= -1
