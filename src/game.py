b8daf0ee55065215046faa7a358fbd84cfad44d5
Pierre Haller <boiteu54@gmail.com>
2019-05-29 09:51:23 +0200

    Fix destination choice

--- src/game.py
+++ src/game.py
@@ -22,25 +22,29 @@ def get_player_choice(question, choices):
 
 
 def play():
-    clear()
     player_team = 1
     board = create_board()
     while True:
+        clear()
+
         movables = get_movables_tiles(board, player_team)
         print_board(board, movables)
 
         movables_choices = list(range(1, len(movables) + 1))
         player_input = get_player_choice('Tile to move : ', movables_choices)
+
+        clear()
+
         (x_start, y_start) = get_coords_from_movables(movables, player_input)
 
         destinations = get_possibles_destinations(board, x_start, y_start, player_team)
-        # clear()
         print_board(board, destinations, (x_start, y_start))
+
         destinations_choices = list(range(1, len(destinations) + 1))
         destination_input = get_player_choice('Destination of the tile : ', destinations_choices)
 
-        (x_end, y_end) = get_coords_from_movables(movables, destination_input)
+        (x_end, y_end) = get_coords_from_movables(destinations, destination_input)
 
         board = move_tile(board, (x_start, y_start), (x_end, y_end), player_team)
+
         player_team *= -1
-        clear()
