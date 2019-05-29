2a0a7c05cf7ae0006cb45a7cd0c36069be0684f6
Luwangel <adrien.amoros@gmail.com>
2019-05-29 10:19:55 +0200

    Manage KeyboardInterrupt properly

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 from game import play
 
-play()
+
+def goodbye():
+    return 'Goodbye!'
+
+
+if __name__ == '__main__':
+    try:
+        play()
+    except KeyboardInterrupt:
+        print('\n' + goodbye())
+        sys.exit(0)
